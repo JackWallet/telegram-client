f7fa0eeae7660d26db78daf67d5d3430ffae165f
InWamos <mykyta.pidgornyi@gmail.com>
2025-01-30 23:55:55 +0200

    Add router inclusion to the dispatcher

--- client/src/root/dispatcher.py
+++ client/src/root/dispatcher.py
@@ -1,6 +1,8 @@
 from aiogram import Dispatcher
+from web_application.router import web_app_router
+
 
 def get_dispatcher() -> Dispatcher:
     dp = Dispatcher()
-
+    dp.include_routers(web_app_router)
     return dp
