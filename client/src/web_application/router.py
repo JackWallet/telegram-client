d1fccd9e89dec0ffebc80fe729e4b19f2701d540
InWamos <mykyta.pidgornyi@gmail.com>
2025-01-30 23:52:57 +0200

    FIx typo

--- client/src/web_application/router.py
+++ client/src/web_application/router.py
@@ -4,6 +4,6 @@ from aiogram.filters.command import CommandStart
 
 web_app_router = Router(name=__name__)
 
-@web_app_router.message(CommandStart, flags={"rate_limit": {"rate": 5}})
+@web_app_router.message(CommandStart(), flags={"rate_limit": {"rate": 5}})
 async def handle_start(message: Message) -> Message:
     return await message.answer(text="Hello")
