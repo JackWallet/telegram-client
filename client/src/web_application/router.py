8e18ed81af3da7655195c09edfe4967447753860
InWamos <mykyta.pidgornyi@gmail.com>
2025-01-30 23:46:24 +0200

    Start command handler

---
+++ client/src/web_application/router.py
@@ -0,0 +1,9 @@
+from aiogram import Router
+from aiogram.types.message import Message
+from aiogram.filters.command import CommandStart
+
+web_app_router = Router(name=__name__)
+
+@web_app_router.message(CommandStart, flags={"rate_limit": {"rate": 5}})
+async def handle_start(message: Message) -> Message:
+    return await message.answer(text="Hello")
